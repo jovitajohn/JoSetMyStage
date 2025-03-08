4d938437c025a8bbde188736d285c0b5399c806f
JovitaJohn <jovitajohn641@gmail.com>
2025-03-08 10:17:32 +0000

    Signup UI completed with fields verification

--- app/signup.tsx
+++ app/signup.tsx
@@ -1,7 +1,13 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image, Alert } from 'react-native';
+import React, { useState,useEffect } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image, Alert,ScrollView } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
-import { useRouter } from 'expo-router';
+import { useRouter,useNavigation  } from 'expo-router';
+import { TextInputMask } from 'react-native-masked-text';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+import { ThemedView } from '@/components/ThemedView';
+
+
 
 export default function Signup() {
 
@@ -11,19 +17,63 @@ export default function Signup() {
   const [email, setEmail] = useState('');
   const [confirmEmail, setConfirmEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [confPassword, setConfPassword] = useState('');
   const [dob, setDob] = useState('');
-  const [address, setAddress] = useState('');
+  const [address1, setAddress1] = useState('');
+  const [address2, setAddress2] = useState('');
+  const [city, setCity] = useState('');
+  const [country, setCountry] = useState('');
   const [mobileNumber, setMobileNumber] = useState('');
   const [profilePic, setProfilePic] = useState('');
+  
+
+        const navigation = useNavigation();
+      
+        useEffect(() => {
+           navigation.setOptions({ 
+                   headerShown: true,
+                   headerStyle: {
+                   shadowColor: 'transparent',  // Remove shadow on iOS
+                   elevation: 0,  // Remove shadow on Android
+                   //opacity: .4,
+                   backgroundColor: 'white', // Optional: Keep header background transparent
+                   height: 100,
+                 }, 
+                 headerTitleAlign: 'start',
+                 headerTitleStyle: {
+                   fontSize: 18,
+                   fontWeight: 'bold',
+                   lineHeight: 100, // Matches header height
+                   flex: 1, 
+                 },
+                 headerTitle: () => (
+                   <View style={{ flexDirection: 'row',  justifyContent: 'space-between', width: '100%' }}>
+                     <Text style={{ fontSize: 18, fontWeight: 'bold',  flex: 1 }}>Sign Up</Text>
+                     <Icon
+                       //name="log-out-outline"
+                       size={24}
+                       color="#00adf5"
+                       style={{ marginRight: 15 }}
+                       onPress={handleSignup}
+                     />
+                   </View>
+                 ),
+               
+                 headerTintColor: '#000000',});
+               },  [navigation]);
+
 
   const handleSignup = () => {
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
     // Basic validation example
-    if (!name || !email || !confirmEmail || !password || !dob || !address || !mobileNumber) {
+    if (!name || !email || !confirmEmail || !password || !dob || !address1 || !address2 || !city || !country || !mobileNumber) {
       Alert.alert('Error', 'All fields are required!');
       return;
     }
-    if (email !== confirmEmail) {
-      Alert.alert('Error', 'Emails do not match!');
+    if (email !== confirmEmail || !emailRegex.test(email) || password.length < 3 || password !== confPassword) {
+      Alert.alert('Error', 'Please enter valid details!');
       return;
     }
 
@@ -50,8 +100,11 @@ export default function Signup() {
   };
 
   return (
+       <SafeAreaProvider>
+       <SafeAreaView style={{flex:1,backgroundColor:'white',marginTop:25}}>
+       <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <ThemedView style={styles.container}>
     <View style={styles.container}>
-      <Text style={styles.title}>Sign Up</Text>
 
       {/* Profile Picture */}
       <TouchableOpacity onPress={handleProfilePicUpload}>
@@ -66,6 +119,9 @@ export default function Signup() {
       <TextInput
         style={styles.input}
         placeholder="Name"
+        multiline={false}
+        maxLength={50} 
+        numberOfLines={1}
         value={name}
         onChangeText={setName}
       />
@@ -73,6 +129,9 @@ export default function Signup() {
         style={styles.input}
         placeholder="Email"
         value={email}
+        multiline={false}
+        scrollEnabled={true}
+        numberOfLines={1} 
         onChangeText={setEmail}
         keyboardType="email-address"
       />
@@ -87,27 +146,80 @@ export default function Signup() {
         style={styles.input}
         placeholder="Password"
         value={password}
+        multiline={false}
+        maxLength={50} 
+        numberOfLines={1}
         onChangeText={setPassword}
         secureTextEntry
       />
       <TextInput
         style={styles.input}
-        placeholder="Date of Birth (YYYY-MM-DD)"
+        placeholder="Confirm Password"
+        value={password}
+        multiline={false}
+        maxLength={50} 
+        numberOfLines={1}
+        onChangeText={setConfPassword}
+        secureTextEntry
+      />
+      <TextInputMask
+        type={'datetime'}
+        options={{ format: 'DD/MM/YYYY' }}
+        maxLength={10}
+        style={styles.input}
+        placeholder="Date of birth (dd/mm/yyyy)"
+        keyboardType="numeric"
         value={dob}
         onChangeText={setDob}
       />
       <TextInput
         style={styles.input}
-        placeholder="Address"
-        value={address}
-        onChangeText={setAddress}
+        multiline={false}
+        maxLength={50} 
+        scrollEnabled={true}
+        numberOfLines={1} 
+        placeholder="Address line1"
+        value={address1}
+        onChangeText={setAddress1}
+      />
+      <TextInput
+        style={styles.input}
+        multiline={false}
+        maxLength={50}
+        scrollEnabled={true}
+        numberOfLines={1} 
+        value={address2}
+        onChangeText={setAddress2}
+        placeholder="Address line 2"
+      />
+      <TextInput
+        style={styles.input}
+        multiline={false}
+        maxLength={50} 
+        scrollEnabled={true}
+        numberOfLines={1} 
+        value={city}
+        onChangeText={setCity}
+        placeholder="City"
+      />
+      <TextInput
+        style={styles.input}
+        multiline={false}
+        maxLength={50} 
+        scrollEnabled={true}
+        numberOfLines={1} 
+        value={country}
+        onChangeText={setCountry}
+        placeholder="Country"
       />
       <TextInput
         style={styles.input}
         placeholder="Mobile Number"
         value={mobileNumber}
         onChangeText={setMobileNumber}
-        keyboardType="phone-pad"
+        keyboardType="numeric"
+        maxLength={11}
+        multiline={false}
       />
 
       {/* Signup Button */}
@@ -115,6 +227,11 @@ export default function Signup() {
         <Text style={styles.buttonText}>Sign Up</Text>
       </TouchableOpacity>
     </View>
+    </ThemedView>
+    </ScrollView>
+    </SafeAreaView>
+    </SafeAreaProvider>
+   
   );
 }
 
@@ -154,7 +271,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
   },
   button: {
-    backgroundColor: '#007bff',
+    backgroundColor: '#1BE7FF',
     padding: 10,
     borderRadius: 5,
     alignItems: 'center',
